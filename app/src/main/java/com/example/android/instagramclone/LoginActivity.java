366db505e347eea99b53a6f746d50c6a2ade63ca
Braingeyser <evilroy@iinet.net.au>
2020-04-25 22:23:07 +1000

    Implementing sanity checks on user data fields

--- app/src/main/java/com/example/android/instagramclone/LoginActivity.java
+++ app/src/main/java/com/example/android/instagramclone/LoginActivity.java
@@ -3,12 +3,61 @@ package com.example.android.instagramclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.shashank.sony.fancytoastlib.FancyToast;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+	private EditText edtEmailLogin;
+	private  EditText edtPasswordLogin;
+
+	private Button btnSignup;
+	private Button btnLogin;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
+		edtEmailLogin = findViewById(R.id.edtEmailLogin);
+		edtPasswordLogin = findViewById(R.id.edtPasswordLogin);
+		btnSignup = findViewById(R.id.btnSignup);
+		btnLogin = findViewById(R.id.btnLogin);
+
+		btnLogin.setOnClickListener(this);
+		btnSignup.setOnClickListener(this);
+
+		if(ParseUser.getCurrentUser() != null){
+			ParseUser.getCurrentUser().logOut();
+		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()){
+			case R.id.btnLogin:
+				ParseUser.logInInBackground(edtEmailLogin.getText().toString(), edtPasswordLogin.getText().toString(), new LogInCallback() {
+					@Override
+					public void done(ParseUser parseUser, ParseException e) {
+						if(parseUser != null && e == null){
+							FancyToast.makeText(LoginActivity.this,
+									parseUser.getUsername() + R.string.message_suffix_login_successful,
+									Toast.LENGTH_SHORT,
+									FancyToast.SUCCESS,
+									true).show();
+						}
+					}
+				});
+				break;
+			case R.id.btnSignup:
+				break;
+		}
 	}
 }
--- app/src/main/java/com/example/android/instagramclone/SignUpActivity.java
+++ app/src/main/java/com/example/android/instagramclone/SignUpActivity.java
@@ -2,83 +2,103 @@ package com.example.android.instagramclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.parse.FindCallback;
-import com.parse.GetCallback;
 import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-import com.parse.SaveCallback;
 import com.parse.SignUpCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
-import java.util.List;
-
 public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
 
-	private EditText edtEmailSignup;
-	private EditText edtUsernameSignup;
-	private EditText edtPasswordSignup;
-	private Button btnSignup;
-	private Button btnLogin;
+    private EditText edtEmailSignup;
+    private EditText edtUsernameSignup;
+    private EditText edtPasswordSignup;
+    private Button btnSignup;
+    private Button btnLogin;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_sign_up);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
 
-		setTitle(R.string.signup_activity_title);
+        setTitle(R.string.signup_activity_title);
 
-		edtEmailSignup 		= findViewById(R.id.edtEmailSignup);
-		edtUsernameSignup 	= findViewById(R.id.edtUsernameSignup);
-		edtPasswordSignup 	= findViewById(R.id.edtPasswordSignup);
-		btnSignup 			= findViewById(R.id.btnSignup);
-		btnLogin 			= findViewById(R.id.btnLogin);
+        edtEmailSignup = findViewById(R.id.edtEmailLogin);
+        edtUsernameSignup = findViewById(R.id.edtUsernameSignup);
+        edtPasswordSignup = findViewById(R.id.edtPasswordLogin);
+        btnSignup = findViewById(R.id.btnSignup);
+        btnLogin = findViewById(R.id.btnLogin);
 
-		btnSignup.setOnClickListener(this);
-		btnLogin.setOnClickListener(this);
-	}
+        btnSignup.setOnClickListener(this);
+        btnLogin.setOnClickListener(this);
+
+        //log out previous user on startup or we will get a session token exception
+        if (ParseUser.getCurrentUser() != null) {
+            ParseUser.getCurrentUser().logOut();
+        }
+    }
 
 
-	@Override
-	public void onClick(View view) {
-		switch (view.getId()){
-			case R.id.btnSignup:
-				final ParseUser appUser = new ParseUser();
-				appUser.setEmail(edtEmailSignup.getText().toString());
-				appUser.setUsername(edtUsernameSignup.getText().toString());
-				appUser.setPassword(edtPasswordSignup.getText().toString());
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSignup:
 
-				appUser.signUpInBackground(new SignUpCallback() {
-					@Override
-					public void done(ParseException e) {
-						if(e == null){
-							FancyToast.makeText(SignUpActivity.this,
-									appUser.getUsername() + R.string.message_suffix_signup_successful,
-									Toast.LENGTH_SHORT,
-									FancyToast.SUCCESS,
-									true).show();
-						}else{
-							FancyToast.makeText(SignUpActivity.this,
-									e.getMessage(),
-									Toast.LENGTH_LONG,	FancyToast.ERROR,true).show();
+                if (edtEmailSignup.getText().toString().equals("") ||
+                        edtUsernameSignup.getText().toString().equals("") ||
+                        edtPasswordSignup.getText().toString().equals("")) {
+                    FancyToast.makeText(SignUpActivity.this,
+                            getString(R.string.message_incomplete_signup_fields),
+                            Toast.LENGTH_SHORT,
+                            FancyToast.INFO,
+                            true).show();
 
-						}
+                } else {
 
-					}
-				});
-				break;
+                    final ParseUser appUser = new ParseUser();
+                    appUser.setEmail(edtEmailSignup.getText().toString());
+                    appUser.setUsername(edtUsernameSignup.getText().toString());
+                    appUser.setPassword(edtPasswordSignup.getText().toString());
 
-			case R.id.btnLogin:
-				break;
-		}
-	}
+                    final ProgressDialog progressDialog = new ProgressDialog(this);
+                    progressDialog.setMessage(getString(R.string.message_prefix_sign_in_progress) + edtUsernameSignup.getText().toString());
+                    progressDialog.show();
+
+                    appUser.signUpInBackground(new SignUpCallback() {
+                        @Override
+                        public void done(ParseException e) {
+                            if (e == null) {
+                                FancyToast.makeText(SignUpActivity.this,
+                                        appUser.getUsername() + R.string.message_suffix_signup_successful,
+                                        Toast.LENGTH_SHORT,
+                                        FancyToast.SUCCESS,
+                                        true).show();
+                            } else {
+                                FancyToast.makeText(SignUpActivity.this,
+                                        e.getMessage(),
+                                        Toast.LENGTH_LONG, FancyToast.ERROR, true).show();
+
+                            }
+                            progressDialog.dismiss();
+
+                        }
+                    });
+                }
+
+                break;
+
+            case R.id.btnLogin:
+                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 }
